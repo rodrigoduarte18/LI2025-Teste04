730380931643643b6e587a6bce29ce29e4eaee05
rodrigoduarte18 <rodrigomatosduarte@ua.pt>
2025-12-04 15:19:08 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 //ficheiro de script
+const form = document.querySelector("form");
+const cancelar = document.getElementById("cancelar");
+
+form.addEventListener("submit", function (e) {
+    if (!form.checkValidity()) {
+        e.preventDefault();
+    }
+    form.classList.add("was-validated");
+});
+
+cancelar.addEventListener("click", function () {
+    form.reset();
+    form.classList.remove("was-validated");
+});
